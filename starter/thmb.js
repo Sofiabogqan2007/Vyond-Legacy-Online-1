a8263a1f9531a47172c17c7c4b76861d0a1677db
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2022-01-11 20:00:48 -0500

    Update thmb.js

--- starter/thmb.js
+++ starter/thmb.js
@@ -3,7 +3,7 @@ module.exports = function (req, res, url) {
 	if (req.method != 'GET' || !url.path.startsWith('/starter_thumbs')) return;
 	starter.thumb(url.path.substr(url.path.lastIndexOf('/') + 1))
 		.then(v => {
-			res.setHeader('Content-Type', 'image/png');
+			res.setHeader(.'Content-Type', 'image/png');
 			res.statusCode = 200; res.end(v);
 		})
 		.catch(() => {
